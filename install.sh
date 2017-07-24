eb7a72eeb2987acdee7b1d8c0225267a2e6d4d63
Ryan Mark <ryan@mrk.cc>
2017-07-24 13:56:39 -0400

    add simple install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ai2html.js /Applications/Adobe\ Illustrator\ CC\ 2017/Presets.localized/en_US/Scripts/ai2html.js
+cp utilities/aifontname.js /Applications/Adobe\ Illustrator\ CC\ 2017/Presets.localized/en_US/Scripts/aifontname.js
